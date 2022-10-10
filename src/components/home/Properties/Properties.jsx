0faba85f53dd315c8de9fd8eb0eed82022828979
Kevin Irungu <kevin.irungu@student.moringaschool.com>
2022-10-10 21:15:53 +0300

    Properties.jsx cleaned up

--- src/components/home/Properties/Properties.jsx
+++ src/components/home/Properties/Properties.jsx
@@ -5,23 +5,16 @@ import "./Properties.css"
 
 
 const Properties = ({ filter }) => {
-
-  
-
-
-
   return (
     <>
       <section className="properties padding">
         <div className="container">
           <Heading title="Properties" subtitle="Select your prefered property here" />
           <PropertiesCard filter={ filter }  />
-          
-           
         </div>
       </section>
     </>
   )
 }
 
-export default Properties
+export default Properties;
