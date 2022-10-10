20c1863785e616f5fbf03574269c46ef62dcb135
Kevin Irungu <kevin.irungu@student.moringaschool.com>
2022-10-10 21:19:15 +0300

    Back.jsx cleaned up

--- src/components/common/Back.jsx
+++ src/components/common/Back.jsx
@@ -4,14 +4,11 @@ const Back = ({ name, title, cover}) => {
   return (
     <>
     <div className="back">
-
         <div className="container">
              <span> { name } </span>
              <h1> { title } </h1>
         </div>
-
         <img src={ cover } alt="" />
-
     </div>
     </>
   )
