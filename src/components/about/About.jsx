6b4817676a087adf5f2585f477819057aadee2bc
Kevin Irungu <kevin.irungu@student.moringaschool.com>
2022-10-10 21:22:05 +0300

    About.jsx cleaned up

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,27 +8,25 @@ import "./About.css";
 const About = () => {
   return (
     <>
-    <section className="about">
-        <Back name="About Us" title="Who We Are?"  cover={image} />
-        <div className="container flex mtop">
-          <div className="left row">
-            <Heading title="Our Agency Story" subtitle="Check out our company story and work process"/>
-            <p>
-              Goldmark Realty was incorporated in the year 2000 with the aim to develop well-designed, competitively priced real estate in Kenya.
-            </p>
-            <p>
-              Our portfolio is well-balanced, comprising a combination of residential, commercial and mixed-use properties. We have managed to gain good reputation in the real estate industry as we deliver a service that provides integrity, professionalism and peace of mind to our clients.
-            </p>
+      <section className="about">
+          <Back name="About Us" title="Who We Are?"  cover={image} />
+          <div className="container flex mtop">
+            <div className="left row">
+              <Heading title="Our Agency Story" subtitle="Check out our company story and work process"/>
+              <p>
+                Goldmark Realty was incorporated in the year 2000 with the aim to develop well-designed, competitively priced real estate in Kenya.
+              </p>
+              <p>
+                Our portfolio is well-balanced, comprising a combination of residential, commercial and mixed-use properties. We have managed to gain good reputation in the real estate industry as we deliver a service that provides integrity, professionalism and peace of mind to our clients.
+              </p>
+            </div>
+            <div className="right row">
+              <img src={ image } alt="" />
+            </div>
           </div>
-
-          <div className="right row">
-            <img src={ image } alt="" />
-          </div>
-
-        </div>
     </section>
     </>
   )
 }
 
-export default About
+export default About;
